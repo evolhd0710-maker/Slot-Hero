35630a9d72f3f2247d8adc708b35ca236532dcf1
unknown <evolhd0710@gmail.com>
2026-05-26 00:53:39 +0900

    스킬 효과 데이터 업데이트 내일 파서 구현해서 스킬 자동으로 구현되도록 할거임

--- Assets/Skills/NewSkillTest/SkillEffect.cs
+++ Assets/Skills/NewSkillTest/SkillEffect.cs
@@ -1,7 +1,48 @@
 using UnityEngine;
 
+public enum DurationType
+{
+    Instant,
+    TurnBased,
+    StackBased
+}
+public enum TickTiming
+{
+    None,
+    TurnStart,
+    TurnEnd
+}
+public enum StackType
+{
+    None,
+    Duration,
+    Intensity
+}
+public enum DecayType
+{
+    None,
+    Constant,
+    Percentage
+}
+
+public enum DecayTiming
+{
+    None,
+    TurnEnd,
+    TurnStart
+}
 [CreateAssetMenu(fileName = "SkillEffect", menuName = "Scriptable Objects/SkillEffect")]
 public abstract class SkillEffect : ScriptableObject
 {
+
+
+    public int effectId;
+    public int effectCode;
+    public DurationType durationType;
+    public int maxStacks;
+    public int DecayValue;
+    public int effectOrder;
+    public Sprite effectIcon;
+
     public abstract void Execute(GameObject caster, GameObject target, int amount);
 }
